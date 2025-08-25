d572dc89b885723ffb905d0e3384d0841cab1f8f
saravanagif <jacksparrow63835@gmail.com>
2025-08-25 20:20:25 +0530

    feat:add datatypes

---
+++ datatype.js
@@ -0,0 +1,20 @@
+let a=10;
+//console.log(a);
+//console.log(typeof(a));
+
+let b;
+let c=undefined;
+
+//console.log(b);
+console.log(typeof(c));
+
+let s1="saravana";
+console.log(typeof(s1));
+let s2="";
+
+let b1=true;
+let b2=false;
+console.log(typeof(b1));
+
+let n=null;
+console.log(typeof(n));
